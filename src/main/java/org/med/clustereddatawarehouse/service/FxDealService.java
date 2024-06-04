0c5cd771f802022c9451139297bfadddbd2ce77d
Mousta-Med <moustamuhammed@gmail.com>
2024-06-04 12:19:27 +0100

    creating fxdeal service layer and related exception

---
+++ src/main/java/org/med/clustereddatawarehouse/service/FxDealService.java
@@ -0,0 +1,9 @@
+package org.med.clustereddatawarehouse.service;
+
+import org.med.clustereddatawarehouse.model.request.FxDealReqDto;
+
+public interface FxDealService {
+
+    FxDealReqDto save(FxDealReqDto request);
+
+}
---
+++ src/main/java/org/med/clustereddatawarehouse/service/impl/FxDealServiceImpl.java
@@ -0,0 +1,31 @@
+package org.med.clustereddatawarehouse.service.impl;
+
+import org.med.clustereddatawarehouse.exception.ResourceAlreadyExistException;
+import org.med.clustereddatawarehouse.model.entity.FxDeal;
+import org.med.clustereddatawarehouse.model.request.FxDealReqDto;
+import org.med.clustereddatawarehouse.repository.FxDealRepository;
+import org.med.clustereddatawarehouse.service.FxDealService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class FxDealServiceImpl implements FxDealService {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private FxDealRepository fxDealRepository;
+
+
+    @Override
+    public FxDealReqDto save(FxDealReqDto request) {
+        if(!fxDealRepository.existsById(request.getId())){
+            return modelMapper.map(fxDealRepository.save(modelMapper.map(request, FxDeal.class)), FxDealReqDto.class);
+        }else {
+            throw new ResourceAlreadyExistException(request.getId());
+        }
+    }
+}

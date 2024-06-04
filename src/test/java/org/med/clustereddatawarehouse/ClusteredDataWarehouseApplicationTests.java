44a56f55d680de3bc1e94836863790e125ac012e
Mousta-Med <moustamuhammed@gmail.com>
2024-06-04 09:59:38 +0100

    first commit

---
+++ src/test/java/org/med/clustereddatawarehouse/ClusteredDataWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.med.clustereddatawarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusteredDataWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
